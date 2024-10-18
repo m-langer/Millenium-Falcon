52e2fa1d86465cf2fad103eaf5d0945d03057bd2
pinzgauner <pinzgauner@gmail.com>
2024-10-18 10:02:09 +0000

    test

--- bauer/main.tf
+++ bauer/main.tf
@@ -7,3 +7,16 @@ resource "azurerm_resource_group" "rg_bauer" {
     owner = "andreas.bauer@redbull.com"
       }
 }
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet-bauer"
+  resource_group_name = azurerm_resource_group.rg_bauer.name
+  location            = azurerm_resource_group.rg_bauer.location
+  address_space       = ["10.10.10.0/24"]
+  
+tags = {
+    owner = "andreas.bauer2@redbull.com"
+      }
+
+}
