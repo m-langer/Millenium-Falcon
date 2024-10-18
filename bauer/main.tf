632f8cb36b6ac2b32f4a01344f58fd5c80e7c623
pinzgauner <pinzgauner@gmail.com>
2024-10-18 11:19:04 +0000

    added vm

--- bauer/main.tf
+++ bauer/main.tf
@@ -22,6 +22,7 @@ tags = {
       }
 
 
+
 }
 
   resource "azurerm_subnet" "sn-bauer" {
@@ -32,4 +33,39 @@ tags = {
 
 
 
+}
+
+resource "azurerm_network_interface" "nic-bauer" {
+  name                = "nic-bauer"
+  location            = azurerm_resource_group.rg_bauer.location
+  resource_group_name = azurerm_resource_group.rg_bauer.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn-bauer.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-bauer-2" {
+  name                = "vm-bauer-2"
+  resource_group_name = azurerm_resource_group.rg_bauer.name
+  location            = azurerm_resource_group.rg_bauer.location
+  size                = "Standard_F2"
+  disable_password_authentication = false
+  admin_username      = "bauer"
+  admin_password = "BauerBauer1234"
+  network_interface_ids = [azurerm_network_interface.nic-bauer.id,  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
