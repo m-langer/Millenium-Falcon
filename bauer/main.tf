601b5c21d062b970494a7e501ca2135bf4206ef9
pinzgauner <pinzgauner@gmail.com>
2024-10-18 12:43:50 +0200

    add subnet (#14)
    * change rg
    
    * test
    
    * added subnet

--- bauer/main.tf
+++ bauer/main.tf
@@ -6,4 +6,30 @@ resource "azurerm_resource_group" "rg_bauer" {
   tags = {
     owner = "andreas.bauer@redbull.com"
       }
+
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet-bauer" {
+  name                = "vnet-bauer"
+  resource_group_name = azurerm_resource_group.rg_bauer.name
+  location            = azurerm_resource_group.rg_bauer.location
+  address_space       = ["10.10.10.0/24"]
+ # dns_servers = "8.8.8.8"
+
+tags = {
+    owner = "andreas.bauer@redbull.com"
+      }
+
+
+}
+
+  resource "azurerm_subnet" "sn-bauer" {
+  name                 = "sn-bauer"
+  resource_group_name  = azurerm_resource_group.rg_bauer.name
+  virtual_network_name = azurerm_virtual_network.vnet-bauer.name
+  address_prefixes     = ["10.10.10.0/25"]
+
+
+
 }
