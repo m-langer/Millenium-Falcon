793641f5908fe57bafaf3813d769137256a181aa
waynewoodroffe <waynewoodroffe@hotmail.com>
2024-10-25 10:34:08 +0200

    add vm (#18)

--- ObiWan/main.tf
+++ ObiWan/main.tf
@@ -30,3 +30,39 @@ resource "azurerm_subnet" "confusing" {
 }
 
 }
+resource "azurerm_network_interface" "confusing" {
+  name                = "confusing-nic"
+  location            = azurerm_resource_group.confusing.location
+  resource_group_name = azurerm_resource_group.confusing.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.confusing.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "confusing" {
+  name                = "confusing-machine"
+  resource_group_name = azurerm_resource_group.confusing.name
+  location            = azurerm_resource_group.confusing.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  disable_password_authentication = false
+  admin_password = "ThisisLondon56"
+  network_interface_ids = [
+    azurerm_network_interface.confusing.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
