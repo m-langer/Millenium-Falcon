14e1a6f42769b221192c475b7efa1fd7ec9c4f36
Moritz Langer <85168282+m-langer@users.noreply.github.com>
2024-10-25 09:20:39 +0000

    add tf cloud to provider

--- R2D2/provider.tf
+++ R2D2/provider.tf
@@ -1,6 +1,13 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
 terraform {
+  cloud {
+    organization = "m-langer-org"
+    workspaces {
+      name = "Millenium-Falcon"
+      }
+  }
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
